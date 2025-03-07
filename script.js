f3f7826da0c5984b48689f9081a72d911cbe27e4
yk-david <81497136+yk-david@users.noreply.github.com>
2025-03-07 04:40:11 +0000

    adjusting navigation bar opacity

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+// Toggle navigation
 document.querySelector('.nav-toggle').addEventListener('click', () => {
   try {
     document.querySelector('.nav').classList.toggle('nav-hidden');
   } catch (error) {
     console.error('Something goes wrong...'); // Log any error
   }
+});
+
+// Control navigation opacity when scroll
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  let isScrolling; 
+  
+  header.style.opacity = '0.85';
+
+  // Clear previous timeout to prevent flickering
+  clearTimeout(isScrolling);
+
+  // Set timeout to restore opacity after user stops scrolling
+  isScrolling = setTimeout(() => {
+    header.style.opacity = '1';
+  }, 200); // 200ms delay after scroll stops
 });
